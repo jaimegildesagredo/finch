07ad75f011b2bc6169d0626f73502919905d7034
Paul Melnikow <email@paulmelnikow.com>
2014-09-29 22:52:13 -0400

    Allow configuring a base URL

--- tests/unit/test_session.py
+++ tests/unit/test_session.py
@@ -40,6 +40,19 @@ class TestSession(object):
             callback=CALLBACK
         ))
 
+    def test_when_base_url_set_then_calls_http_client_fetch_with_base_url(self):
+        with Spy(httpclient.HTTPClient()) as http_client:
+            session = Session(http_client, base_url='https://example.com:12345')
+
+        session.fetch('/users?type=json', callback=CALLBACK, params={'is_admin': 'true'})
+
+        assert_that(http_client.fetch, called().with_args(
+            has_properties(
+                url='https://example.com:12345/users?type=json&is_admin=true'
+            ),
+            callback=CALLBACK
+        ))
+
 class TestSessionWithBasicAuth(object):
     def test_when_auth_is_username_and_password_tuple_then_session_uses_basic_auth(self):
         session = Session(Stub(), auth=(u'root', u'toor'))
